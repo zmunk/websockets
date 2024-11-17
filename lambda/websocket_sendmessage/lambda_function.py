8d693bd566119ecdb748d5ca7037d9709f886e42
zmunk <>
2024-11-17 15:11:57 +0300

    send welcome message on connect

--- lambda/websocket_sendmessage/lambda_function.py
+++ lambda/websocket_sendmessage/lambda_function.py
@@ -5,23 +5,24 @@ from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(os.environ["CONNECTIONS_TABLE"])
+apigateway = None
 
 
 def lambda_handler(event, context):
+    global apigateway
+
     conn_id = event["requestContext"]["connectionId"]
     domain_name = event["requestContext"]["domainName"]
     stage = event["requestContext"]["stage"]
-
-    print(f"{conn_id = }")
-
     endpoint = f"https://{domain_name}/{stage}"
-    print(f"{endpoint = }")
-
+    apigateway = boto3.client("apigatewaymanagementapi", endpoint_url=endpoint)
     body = json.loads(event["body"])
-    print(f"{body = }")
-
     event_type = body["type"]
     event_body = body["body"]
+
+    print(f"{conn_id = }")
+    print(f"{endpoint = }")
+    print(f"{body = }")
 
     if event_type == "message":
         try:
@@ -31,7 +32,6 @@ def lambda_handler(event, context):
             print(f"Error getting connection: {str(e)}")
             sender_name = None
         broadcast_user_message(
-            endpoint,
             event_body["message"],
             sender_name,
             skip=[conn_id],
@@ -48,11 +48,8 @@ def lambda_handler(event, context):
         except Exception as e:
             print(f"Error getting connection: {str(e)}")
 
-        broadcast_server_message(
-            endpoint,
-            f"{username} has entered the chat.",
-            skip=[conn_id],
-        )
+        send_message(create_server_message(f"Welcome, {username}!"), conn_id)
+        broadcast_server_message(f"{username} has entered the chat.", skip=[conn_id])
 
     else:
         print(f"unknown event type: {event_type}")
@@ -61,7 +58,7 @@ def lambda_handler(event, context):
     return {}
 
 
-def broadcast_user_message(endpoint, message, sender_name, skip=[]):
+def broadcast_user_message(message, sender_name, skip=[]):
     data = {
         "type": "user_message",
         "body": {
@@ -69,37 +66,45 @@ def broadcast_user_message(endpoint, message, sender_name, skip=[]):
             "sender": sender_name,
         },
     }
-    broadcast_message(endpoint, data, skip=skip)
+    broadcast_message(data, skip=skip)
 
 
-def broadcast_server_message(endpoint, message, skip=[]):
-    data = {
+def create_server_message(message):
+    return {
         "type": "server_message",
         "body": {"message": message},
     }
-    broadcast_message(endpoint, data, skip=skip)
 
 
-def broadcast_message(endpoint, data, skip=[]):
-    apigateway = boto3.client("apigatewaymanagementapi", endpoint_url=endpoint)
+def broadcast_server_message(message, skip=[]):
+    data = create_server_message(message)
+    broadcast_message(data, skip=skip)
 
+
+def broadcast_message(data, skip=[]):
     for connection in get_connections():
         connection_id = connection["connection_id"]
 
         if connection_id in skip:
             continue
 
-        try:
-            apigateway.post_to_connection(
-                ConnectionId=connection_id,
-                Data=json.dumps(data),
-            )
-        except ClientError as e:
-            if e.response["Error"]["Code"] == "GoneException":
-                # Connection is no longer valid, delete it from the table
-                table.delete_item(Key={"connection_id": connection_id})
-            else:
-                print(f"Error sending message to {connection_id}: {str(e)}")
+        send_message(data, connection_id)
+
+
+def send_message(data, connection_id):
+    assert apigateway is not None
+
+    try:
+        apigateway.post_to_connection(
+            ConnectionId=connection_id,
+            Data=json.dumps(data),
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "GoneException":
+            # Connection is no longer valid, delete it from the table
+            table.delete_item(Key={"connection_id": connection_id})
+        else:
+            print(f"Error sending message to {connection_id}: {str(e)}")
 
 
 def get_connections():
